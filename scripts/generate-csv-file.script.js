698b3266c94a46a5cd83df560271e97d04915909
Orim Dominic <orimdominic@gmail.com>
2022-07-13 23:31:38 +0100

    ft: include csv-generating script

--- scripts/generate-csv-file.script.js
+++ scripts/generate-csv-file.script.js
@@ -5,7 +5,9 @@
  */
 
 const fs = require("fs");
+const fsPromise = require("fs/promises")
 const path = require("path");
+let converter = require('json-2-csv');
 
 async function* walk(dir) {
   for await (const d of await fs.promises.opendir(dir)) {
@@ -16,7 +18,29 @@ async function* walk(dir) {
 }
 
 (async () => {
+  let all = []
   for await (const file of walk(`./states/`)) {
-    // @TODO Write code here
+    if (file.endsWith('/units/index.json')) {
+      const json = JSON.parse(await fsPromise.readFile(file, { encoding: 'utf8' }))
+      all = [...all, ...json]
+    }
   }
+
+  converter.json2csv(all, async (err, csv) => {
+    if (err) {
+      return console.error(err)
+    }
+    await fsPromise.writeFile(path.join(process.cwd(), 'polling-units.csv'), csv)
+  },
+    {
+      emptyFieldValue: "",
+      keys: [
+        "name",
+        "ward_name",
+        "local_government_name",
+        "state_name",
+        "location.latitude",
+        "location.longitude"
+      ]
+    })
 })();
