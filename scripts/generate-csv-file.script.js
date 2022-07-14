0974d47364678850dcaae75abc93f359758e983e
Orim Dominic <orimdominic@gmail.com>
2022-07-14 11:06:48 +0100

    ft: update PU stats in readme

--- scripts/generate-csv-file.script.js
+++ scripts/generate-csv-file.script.js
@@ -17,6 +17,75 @@ async function* walk(dir) {
   }
 }
 
+/**
+ * @param {Object[]} units
+ * @param {string} units[].id
+ * @param {string} units[].ward_id
+ * @param {string} units[].local_government_id
+ * @param {string} units[].state_name
+ */
+function generatePUTable(units) {
+  let unitsMap = {}
+  let totalUnits = 0
+
+  for (const unit of units) {
+    if (!unitsMap[unit.state_name]) {
+      unitsMap[unit.state_name] = {}
+      unitsMap[unit.state_name]['pollingUnits'] = 0
+    }
+
+    if (!unitsMap[unit.state_name][unit.local_government_id]) {
+      unitsMap[unit.state_name]['lgas'] = 1
+      unitsMap[unit.state_name][unit.local_government_id] = {}
+    } else {
+      unitsMap[unit.state_name]['lgas']++
+    }
+
+    if (!unitsMap[unit.state_name][unit.local_government_id][unit.ward_id]) {
+      unitsMap[unit.state_name]['wards'] = 1
+      unitsMap[unit.state_name][unit.local_government_id][unit.ward_id] = {}
+    } else {
+      unitsMap[unit.state_name]['wards']++
+    }
+
+    if (!unitsMap[unit.state_name][unit.local_government_id][unit.ward_id][unit.id]) {
+      unitsMap[unit.state_name][unit.local_government_id][unit.ward_id][unit.id] = true
+      unitsMap[unit.state_name]['pollingUnits']++
+      totalUnits++
+    }
+  }
+
+  let tableInfo = `
+Total polling units: ${totalUnits}
+
+| State | LGAs | Wards | Polling Units |
+| ----- | ---- | ----- | ------- |`;
+
+  for (const state of Object.keys(unitsMap)) {
+    tableInfo += `
+| ${state} | ${unitsMap[state]['lgas']} | ${unitsMap[state]['wards']} | ${unitsMap[state]['pollingUnits']} |`
+  }
+
+  return tableInfo
+}
+
+/**
+ * @param {string} tableInfo
+ */
+async function updateReadmePUTable(tableInfo) {
+  const readmePath = path.join(process.cwd(), 'README.md')
+  const readme = await fsPromise.readFile(readmePath, {
+    encoding: 'utf8'
+  })
+  const [firstPart] = readme.split('### Polling Unit Stats')
+  const [_, lastPart] = readme.split('<!-- End of PU stats -->')
+
+  const updatedReadmeText = firstPart + '### Polling Unit Stats\n'
+    + tableInfo + '\n<!-- End of PU stats -->' + lastPart;
+
+  await fsPromise.writeFile(readmePath, updatedReadmeText)
+}
+
 (async () => {
   let all = []
   for await (const file of walk(`./states/`)) {
@@ -25,6 +94,8 @@ async function* walk(dir) {
       all = [...all, ...json]
     }
   }
+  const tableInfo = generatePUTable(all)
+  await updateReadmePUTable(tableInfo)
 
   converter.json2csv(all, async (err, csv) => {
     if (err) {
